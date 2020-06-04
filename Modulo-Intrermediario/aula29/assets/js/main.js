ca35b0bcb6004a40313e855caa90b17efa1e7b16
Gabryel Boeira <gabryel392@gmail.com>
2020-06-04 19:56:09 -0300

    Outra opção de solução

--- Modulo-Intrermediario/aula29/assets/js/main.js
+++ Modulo-Intrermediario/aula29/assets/js/main.js
@@ -1,51 +1,22 @@
 function exibirData(){
-    const conteiner = document.querySelector('.container');
+    const conteinerH1 = document.querySelector('.container h1');
 
-    conteiner.appendChild(createTexteInHtml());
+    conteinerH1.innerHTML = formaDataTexto();
     
     function formaDataTexto(){
         let data = new Date();
         let diaTexto =  getDiaSemana(data.getDay());
-        let mesTexto = getMes(data.getMonth());
+        let mesTexto = getNomeMes(data.getMonth());
         
         return `${diaTexto}, ${data.getDate()} de ${mesTexto} de ${data.getFullYear()} ás ${data.getHours()}:${data.getMinutes()}`;
     }
 
-    function createTexteInHtml(){
-        const h1 = createH1();
-        h1.classList.add('h1-hora');
-        h1.innerHTML = formaDataTexto();
-        return h1;
-    }
-
-    function createH1(){
-        return document.createElement('h1'); 
-    }
-
     function getDiaSemana(numDiaSemana){
-        
-        switch (numDiaSemana) {
-            case 0:
-                return 'Domingo';
-            case 1:
-                return 'Segunda-Feira';                 
-            case 2:
-                return 'Terça-Feira';
-            case 3:                 
-                return 'Quarta-Feira';
-            case 4:
-                return 'Quinta-Feira';
-            case 5:
-                return 'Sexta-Feira';               
-            case 6:
-                return'Sabado';                 
-            default:
-                return '';
-        }    
+        const diasSemana = ['Domingo','Segunda-Feira','Terça-Feira','Quarta-Feira','Quinta-Feira','Sexta-Feira', 'Sabado']
+        return diasSemana[numDiaSemana];
     } 
 
-    function getMes(numMes){
-
+    function getNomeMes(numMes){
         switch (numMes) {
             case 0:
                 return 'Janeiro';                
@@ -64,7 +35,7 @@ function exibirData(){
             case 7:
                 return 'Agosto'; 
             case 8:
-                return 'Setmbro';   
+                return 'Setembro';   
             case 9:
                 return 'Outubro';                 
             case 10:
