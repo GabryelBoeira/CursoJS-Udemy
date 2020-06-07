7b95f985e04a0c71a7b55cd1c504481997a85ce8
Gabryel Boeira <gabryel392@gmail.com>
2020-06-07 19:23:44 -0300

    Exercicio

---
+++ main.js
@@ -0,0 +1,10 @@
+// Jeito um
+//function isPaisagem(altura, largura){
+//    return largura > altura;
+//}
+
+const isPaisagem = (altura, largura) => {
+    return largura > altura;
+}
+
+console.log(isPaisagem(1920, 1080));
