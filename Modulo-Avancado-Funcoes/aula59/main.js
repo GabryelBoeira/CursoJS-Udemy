571f02fa52c2d03e83f5363fa3ee917a688d175f
Gabryel Boeira <gabryel392@gmail.com>
2020-06-10 19:18:16 -0300

    Funçoes geradoras

--- Modulo-Avancado-Funcoes/aula59/main.js
+++ Modulo-Avancado-Funcoes/aula59/main.js
@@ -1,10 +1,78 @@
-// Funçoes recursivas 
+// Funçoes geradoras a avaliação preguiçosa
 
-function recursiva(max){
-    console.log(max);
-    if(max >= 10 ) return;
-    max++;    
-    recursiva(max);
+function* geradora1(){
+    // similar ao return para esse tipo de função
+    // retorna na seguencia dos yield
+    yield 'Valor 1';
+
+    yield 'Valor 2';
+    
+    yield 'Valor 3';
 }
 
-recursiva(0);
+const g1 = geradora1();
+for(let valor of g1){
+    console.log(valor);
+}
+console.log();
+
+////////////////////////////
+function* geradora2(){
+    // gerador de numeros infinito
+    let i = 0;
+    
+    while(true){
+        yield i;
+        i++;
+    }
+}
+const g2 = geradora2();
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log();
+
+///////////////////////
+//delegação de tarefas entre geradoras 
+function* geradora3(){
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4(){
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+for(let valor of g4){
+    console.log(valor);
+}
+console.log();
+//////////////////
+
+function* geradora5(){
+   
+    yield function(){
+        console.log('Vim yield 01');
+    };
+
+    return function(){
+        console.log('Vim do return');
+    };
+    // qualquer coisa abaixo do return não sera execudado 
+    yield function(){
+        console.log('Vim yield 03');
+    };
+}
+
+const g5 = geradora4();
+const funcao1 = g5.next().value;
+const funcao2 = g5.next().value;
+funcao1();
+funcao2();
---
+++ Modulo-Avancado-Funcoes/aula60/main.js
@@ -0,0 +1,10 @@
+// Funçoes recursivas 
+
+function recursiva(max){
+    console.log(max);
+    if(max >= 10 ) return;
+    max++;    
+    recursiva(max);
+}
+
+recursiva(0);
