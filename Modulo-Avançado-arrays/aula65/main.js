3eca62c79313bc8432db97a15e81f2187337a855
Gabryel Boeira <gabryel392@gmail.com>
2020-06-18 14:13:26 -0300

    Utilização da combinação do filter, map e reduce

---
+++ Modulo-Avançado-arrays/aula65/main.js
@@ -0,0 +1,14 @@
+// Utilização da combinação do filter, map e reduce 
+
+// Retornae a soma do dobro de todos os numeros pares
+// -> filtrar numeros pares
+// -> Dobrar numeros pares
+// -> Reduzir numeros pares (Somar os numeros )
+
+const numeros = [45, 54 ,8 ,85, 9, 3, 89, 42, 10, 59, 33, 7, 2, 1, 0];
+const totalDobroPares = numeros
+                        .filter((valor) => valor % 2 === 0)
+                        .map((valor) => valor * 2)
+                        .reduce((acumulador, valor) => acumulador += valor);
+
+console.log(totalDobroPares)
