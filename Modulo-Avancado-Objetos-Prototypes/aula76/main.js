53f7f57ceee4cf25903b6f095bf08eb92ea1de72
Gabryel Boeira <gabryel392@gmail.com>
2020-06-26 21:01:19 -0300

    Terminio de polimorfismo

--- Modulo-Avancado-Objetos-Prototypes/aula76/main.js
+++ Modulo-Avancado-Objetos-Prototypes/aula76/main.js
@@ -9,11 +9,49 @@ function Conta(agencia, conta, saldo){
 
 Conta.prototype.sacar = function(valor) {
     if(this.saldo < valor){
-        this.saldo();
-        return ;
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
     }
     this.saldo -= valor;
-     
+    this.verSaldo();
 };
-Conta.prototype.depositar = function(valor) {};
-Conta.prototype.saldo = function() {};
+
+Conta.prototype.depositar = function(valor) {
+    this.saldo += valor;
+    this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function() {
+    console.log(`Ag./c : ${this.agencia}/${this.conta} | Saldo: ${this.saldo.toFixed(2)}`);
+};
+
+// Conta corrente
+function ContaCorrente( agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor) {
+    if(( this.limite + this.saldo) < valor){
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+const conta1 = new ContaCorrente(10, 2321, 0, 1000);
+conta1.sacar(500);
+
+// Conta Poupança
+function ContaPoupanca( agencia, conta, saldo){
+    Conta.call(this, agencia, conta, saldo);
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+const conta1 = new ContaCorrente(10, 2321, 1000);
